4533c13cb13635ecabf49077b7adf173941491e6
ravichandran22 <ravichandran220799@gmail.com>
2025-10-07 21:11:44 +0530

    Update .env file

--- src/utils/youtubeAPI.js
+++ src/utils/youtubeAPI.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
-// const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-const API_KEY = "AIzaSyCSw8ij1kF15nVD6Zy3T_U_GdDtrGzvKUo";
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+// const API_KEY = "AIzaSyCSw8ij1kF15nVD6Zy3T_U_GdDtrGzvKUo";
 
 export const fetchTrendingVideos = async () => {
   try {
